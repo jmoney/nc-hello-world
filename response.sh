e90a18b74f745774d110a8d0c96773327943a66b
jmoney <jon.monette@gmail.com>
2022-01-06 09:02:53 -0600

    possible fix

--- response.sh
+++ response.sh
@@ -3,4 +3,4 @@
 set -x
 
 content="Hello World!\n${TEXT}\n$(date)"
-echo "HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/plain\nContent-Length: $((${#content}-1))\n\n${content}"
+echo "HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/plain\nContent-Length: $((${#content}))\n\n${content}"
